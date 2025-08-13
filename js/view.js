fa95cd9887ef3c92abbe4a37ef6da2d5d2bf27fa
Inanden <147476553+Inanden@users.noreply.github.com>
2025-08-13 16:20:18 +0100

    Ajuste no menu lista

--- js/view.js
+++ js/view.js
@@ -1,3 +1,3 @@
 export const description =
   "O mundo Pokémon é um universo vibrante e multifacetado, onde criaturas extraordinárias";
-export const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=12";
+export const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=82";
